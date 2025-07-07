5e20715a36d993500ff9df47f92ac7316b6974c5
Style-of-Aries <ductrong34end@gmail.com>
2025-07-07 15:56:45 +0700

    db

--- db/webmusic.sql
+++ db/webmusic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th7 05, 2025 lúc 11:42 PM
+-- Thời gian đã tạo: Th7 07, 2025 lúc 10:56 AM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.2.12
 
@@ -24,6 +24,55 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `albums`
+--
+
+CREATE TABLE `albums` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `artist_id` int(11) DEFAULT NULL,
+  `release_date` date DEFAULT NULL,
+  `cover_image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `albums`
+--
+
+INSERT INTO `albums` (`id`, `name`, `artist_id`, `release_date`, `cover_image`) VALUES
+(1, 'Chúng Ta', 1, '2020-07-01', 'images/albums/chung-ta.jpg'),
+(2, 'Mưa', 2, '2017-09-15', 'images/albums/mua.jpg'),
+(3, 'Hơn Cả Yêu', 3, '2020-02-14', 'images/albums/hon-ca-yeu.jpg'),
+(4, '365 Collection', 4, '2016-06-01', 'images/albums/365collection.jpg'),
+(5, 'Có Em Chờ', 5, '2017-05-10', 'images/albums/co-em-cho.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `artists`
+--
+
+CREATE TABLE `artists` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `bio` text DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `artists`
+--
+
+INSERT INTO `artists` (`id`, `name`, `bio`, `image`) VALUES
+(1, 'Sơn Tùng M-TP', 'Ca sĩ nổi tiếng với phong cách hiện đại, sáng tác nhiều bản hit như Lạc Trôi, Chạy Ngay Đi.', 'images/son-tung.jpg'),
+(2, 'Hương Tràm', 'Giọng ca nội lực của dòng nhạc ballad, nổi tiếng với ca khúc Em Gái Mưa.', 'images/huong-tram.jpg'),
+(3, 'Đức Phúc', 'Ca sĩ trẻ trưởng thành từ The Voice, nổi bật với giọng hát cảm xúc.', 'images/duc-phuc.jpg'),
+(4, '365DaBand', 'Nhóm nhạc nam từng gây sốt với Bống Bống Bang Bang.', 'images/365daband.jpg'),
+(5, 'Min', 'Nữ ca sĩ cá tính với loạt hit như Có Em Chờ, Trên Tình Bạn Dưới Tình Yêu.', 'images/min.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `favorites`
 --
 
@@ -36,23 +85,72 @@ CREATE TABLE `favorites` (
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `genres`
+--
+
+CREATE TABLE `genres` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `genres`
+--
+
+INSERT INTO `genres` (`id`, `name`) VALUES
+(1, 'Pop'),
+(2, 'Ballad'),
+(3, 'Dance'),
+(4, 'R&B'),
+(5, 'Rap/Hip-hop'),
+(6, 'Rock'),
+(7, 'Indie'),
+(8, 'EDM'),
+(9, 'Acoustic'),
+(10, 'Folk');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `playlists`
+--
+
+CREATE TABLE `playlists` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `playlist_songs`
+--
+
+CREATE TABLE `playlist_songs` (
+  `playlist_id` int(11) NOT NULL,
+  `song_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `songs`
 --
 
 CREATE TABLE `songs` (
-  `id` int(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `fileSong` varchar(255) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `artist` varchar(255) NOT NULL
+  `id` int(11) NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `artist_id` int(11) DEFAULT NULL,
+  `album_id` int(11) DEFAULT NULL,
+  `genre_id` int(11) DEFAULT NULL,
+  `file_path` varchar(255) DEFAULT NULL,
+  `cover_image` varchar(255) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `upload_by` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Đang đổ dữ liệu cho bảng `songs`
---
-
-INSERT INTO `songs` (`id`, `name`, `fileSong`, `image`, `artist`) VALUES
-(5, 'Một Năm Mới Bình An', './../public/uploads/audio/song1.mp3', './../public/uploads/img/1nmba.jpg', 'Sơn Tùng Mtp');
 
 -- --------------------------------------------------------
 
@@ -81,6 +179,19 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `avatar`, `sodientho
 --
 
 --
+-- Chỉ mục cho bảng `albums`
+--
+ALTER TABLE `albums`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artist_id` (`artist_id`);
+
+--
+-- Chỉ mục cho bảng `artists`
+--
+ALTER TABLE `artists`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Chỉ mục cho bảng `favorites`
 --
 ALTER TABLE `favorites`
@@ -88,10 +199,34 @@ ALTER TABLE `favorites`
   ADD KEY `song_id` (`song_id`);
 
 --
+-- Chỉ mục cho bảng `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `playlists`
+--
+ALTER TABLE `playlists`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Chỉ mục cho bảng `playlist_songs`
+--
+ALTER TABLE `playlist_songs`
+  ADD PRIMARY KEY (`playlist_id`,`song_id`),
+  ADD KEY `song_id` (`song_id`);
+
+--
 -- Chỉ mục cho bảng `songs`
 --
 ALTER TABLE `songs`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artist_id` (`artist_id`),
+  ADD KEY `album_id` (`album_id`),
+  ADD KEY `genre_id` (`genre_id`),
+  ADD KEY `upload_by` (`upload_by`);
 
 --
 -- Chỉ mục cho bảng `users`
@@ -106,10 +241,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT cho bảng `albums`
+--
+ALTER TABLE `albums`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT cho bảng `artists`
+--
+ALTER TABLE `artists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT cho bảng `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT cho bảng `playlists`
+--
+ALTER TABLE `playlists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT cho bảng `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT cho bảng `users`
@@ -122,11 +281,39 @@ ALTER TABLE `users`
 --
 
 --
+-- Các ràng buộc cho bảng `albums`
+--
+ALTER TABLE `albums`
+  ADD CONSTRAINT `albums_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`id`);
+
+--
 -- Các ràng buộc cho bảng `favorites`
 --
 ALTER TABLE `favorites`
   ADD CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`);
+
+--
+-- Các ràng buộc cho bảng `playlists`
+--
+ALTER TABLE `playlists`
+  ADD CONSTRAINT `playlists_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Các ràng buộc cho bảng `playlist_songs`
+--
+ALTER TABLE `playlist_songs`
+  ADD CONSTRAINT `playlist_songs_ibfk_1` FOREIGN KEY (`playlist_id`) REFERENCES `playlists` (`id`),
+  ADD CONSTRAINT `playlist_songs_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`);
+
+--
+-- Các ràng buộc cho bảng `songs`
+--
+ALTER TABLE `songs`
+  ADD CONSTRAINT `songs_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`id`),
+  ADD CONSTRAINT `songs_ibfk_2` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`),
+  ADD CONSTRAINT `songs_ibfk_3` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`),
+  ADD CONSTRAINT `songs_ibfk_4` FOREIGN KEY (`upload_by`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
